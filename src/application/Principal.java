ac4a73780dd8fed52b57f61f29534053a7912d43
Wiliam Ferreira <wferreirasp@yahoo.com.br>
2020-06-09 11:17:51 -0300

    Alterado o projeto para fins de teste de GitHub

--- src/application/Principal.java
+++ src/application/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Gerenciador de Manutenção");
-
+		System.out.println("Esta é a primeira alteração feita no projeto através do GitHub!");
 	}
 
 }
